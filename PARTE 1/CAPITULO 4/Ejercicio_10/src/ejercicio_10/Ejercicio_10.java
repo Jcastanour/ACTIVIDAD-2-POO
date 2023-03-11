db437fe7b8b23c70b66c6266a1c83c8f8f9efc12
jcastanour <jcastanour@unal.edu.co>
2023-03-11 18:15:08 -0500

    ejercicio 10

---
+++ PARTE 1/CAPITULO 4/Ejercicio_10/src/ejercicio_10/Ejercicio_10.java
@@ -0,0 +1,32 @@
+package ejercicio_10;
+
+import java.util.Scanner;
+
+public class Ejercicio_10 {
+
+    public static void main(String[] args) {
+        int estrato;
+        double patrimonio, pagmat;
+        String ni,nombre;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite el numero de inscripion: ");
+        ni = entrada.nextLine();
+        System.out.print("Digita tu nombre: ");
+        nombre = entrada.nextLine();
+        System.out.print("Digite el patrinomio: ");
+        patrimonio = entrada.nextDouble();
+        System.out.print("Digite el estrato social: ");
+        estrato = entrada.nextInt();
+
+        pagmat = 50000;
+        if ((patrimonio > 2000000) && (estrato > 3)) {
+            pagmat = pagmat + 0.03 * patrimonio;
+        }
+        
+        System.out.println("El estudiante " + nombre + " con numero de identificacion " + ni + " debe pagar $" + pagmat);
+
+    }
+
+}
