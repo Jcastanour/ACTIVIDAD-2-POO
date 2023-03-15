e54229c644f292993b723c6478819ab87862d8eb
jcastanour <jcastanour@unal.edu.co>
2023-03-14 23:50:29 -0500

    Ejercicio 22

---
+++ PARTE 1/CAPITULO 4/Ejercicio_22/src/ejercicio_22/Ejercicio_22.java
@@ -0,0 +1,34 @@
+
+package ejercicio_22;
+
+//@author Juan Pablo
+
+import java.util.Scanner;
+
+public class Ejercicio_22 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double valor, horas, salarioMensual;
+        String nombre;
+       
+        System.out.println("Por favor ingrese el nombre");
+        nombre = input.nextLine();
+       
+        System.out.println("Por favor ingrese el salario(por hora) de "+ nombre);
+        valor = input.nextDouble();
+       
+         System.out.println("Por favor ingrese el número de horas trabajadas");
+        horas = input.nextDouble();
+       
+       
+        salarioMensual = valor*horas;
+       
+        if (salarioMensual>450000) {
+            System.out.println("Nombre: "+nombre+". Salario: "+salarioMensual);              
+        }
+        else {
+            System.out.println("Nombre: "+nombre);
+        }                                      
+    }
+}
