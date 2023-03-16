9dd00177a41e7c28fbafbe16c99f6a713a480027
jcastanour <jcastanour@unal.edu.co>
2023-03-16 16:53:44 -0500

    Trabajo terminado

--- PARTE 2/figuras_geometricas/src/figuras_geometricas/Figuras_geometricas.java
+++ PARTE 2/figuras_geometricas/src/figuras_geometricas/Figuras_geometricas.java
@@ -1,11 +1,52 @@
-
 package figuras_geometricas;
 
-//@author Juan Pablo
+import java.util.Scanner;
+
 public class Figuras_geometricas {
 
     public static void main(String[] args) {
-       
+        Scanner valor = new Scanner(System.in);
+        int radio, BaseRect, AlturaRect, ladoC, baset, alturat;
+
+        // Circulo //
+        System.out.println("Ingrese el valor del radio del circulo: ");
+        radio = valor.nextInt();
+
+        Circulo figura1 = new Circulo(radio);
+        System.out.println("El area del circulo es: " + figura1.calcularArea()); // Area
+        System.out.println("El perimetro del circulo es: " + figura1.calcularperimetro()); //Perimetro
+
+        // Rectangulo//
+        System.out.println("Ingrese el valor de la base del rectangulo: ");
+        BaseRect = valor.nextInt();
+        System.out.println("Ingrese el valor de la altura del rectangulo: ");
+        AlturaRect = valor.nextInt();
+
+        Rectangulo figura2 = new Rectangulo(BaseRect, AlturaRect);
+        System.out.println("El area del rectangulo es: " + figura2.area());
+        System.out.println("El perimetro del rectangulo es: " + figura2.perimetro());
+
+        // Cuadrado //
+        System.out.println("Ingrese el valor del lado del cuadrado: ");
+        ladoC = valor.nextInt();
+
+        Cuadrado figura3 = new Cuadrado(ladoC);
+        System.out.println("El area del cuadrado es: " + figura3.CalcuarArea());
+        System.out.println("El preimtetro del cuadrado es: " + figura3.CalcularPerimetro()); 
+
+        // Triangulo //
+        System.out.println("ingrese el valor de la base del triangulo rectangulo: ");
+        baset = valor.nextInt();
+        System.out.println("ingrese el valor de la altura del triangulo rectangulo: ");
+        alturat = valor.nextInt();
+
+        Triangulo figura4 = new Triangulo(baset, alturat);
+        System.out.println("El area del triangulo es: " + figura4.calcularArea());
+        System.out.println("El perimetro del triangulo es: " + figura4.calcularPerimetro());
+
+        // Tipo de triangulo //
+        figura4.determinarTipoTriangulo();
+
     }
 
 }
---
+++ PARTE 2/figuras_geometricas/src/figuras_geometricas/Triangulo.java
@@ -0,0 +1,34 @@
+package figuras_geometricas;
+
+public class Triangulo {
+
+    int baset, alturat;
+
+    public Triangulo(int baset, int alturat) {
+        this.baset = baset;
+        this.alturat = alturat;
+    }
+
+    double calcularHipotenusa() {
+        return Math.pow(baset * baset + alturat * alturat, 0.5);
+    }
+
+    double calcularArea() {
+        return (baset * alturat / 2);
+    }
+
+    double calcularPerimetro() {
+        return (baset + alturat + calcularHipotenusa());
+    }
+
+    void determinarTipoTriangulo() {
+        if ((baset == alturat) && (baset == calcularHipotenusa()) && (alturat == calcularHipotenusa())) {
+            System.out.println("Es un triangulo equilatero");
+        } else if ((baset != alturat) && (baset != calcularHipotenusa()) && (alturat != calcularHipotenusa())) {
+            System.out.println("Es un triangulo escaleno");
+        } else {
+            System.out.println("Es un triangulo isosceles");
+
+        }
+    }
+}
